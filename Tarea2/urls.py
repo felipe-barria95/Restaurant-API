1b489c8061ab652e78aa1540b529ecbcb68ce08e
felipe-barria95 <frbarria@uc.cl>
2020-04-20 13:32:45 -0400

    static

--- Tarea2/urls.py
+++ Tarea2/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
+
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
+    re_path(r'^favicon\.ico$', favicon_view),
     path('', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
