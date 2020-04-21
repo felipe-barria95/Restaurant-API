2691a25f0648cd2ae730a24f40147b256def6ac8
felipe-barria95 <febamo0@gmail.com>
2020-04-21 13:30:36 -0400

    aaa

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,10 @@ from api import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('hamburguesas/', views.HamburguesaList.as_view()),
-    path('ingredientes/', views.IngredienteList.as_view()),
-    path('hamburguesas/<int:pk>/', views.HamburguesaDetail.as_view()),
-    path('ingredientes/<int:pk>/', views.IngredienteDetail.as_view()),
+    path('hamburguesa/', views.HamburguesaList.as_view()),
+    path('ingrediente/', views.IngredienteList.as_view()),
+    path('hamburguesa/<int:pk>/', views.HamburguesaDetail.as_view()),
+    path('ingrediente/<int:pk>/', views.IngredienteDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
